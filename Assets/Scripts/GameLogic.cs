9564b5d307db76733d30e76531a79a6bb72bc543
Rahul Joshi <rahuljoshi1997@gmail.com>
2016-09-15 16:49:27 -0400

    Have the basic player movement + camera movement. Also have the win sphere and it's script in.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+	bool didPlayerWin = false;
+	public Text winText;
+	public Transform player;
+	public Transform treasure;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (didPlayerWin) {
+			winText.text = "You Win!";
+		} else if ((player.position - treasure.position).magnitude < 5f) {
+			winText.text = "Press [SPACE] to Win";
+			if (Input.GetKey(KeyCode.Space)) {
+				didPlayerWin = true;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		movementSpeed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0f, 0f, movementSpeed) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0f, 0f, 0f);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (0f, 0f, -movementSpeed) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0f, 180f, 0f);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (movementSpeed, 0f, 0f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0f, -90f, 0f);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (-movementSpeed, 0f, 0f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0f, 90f, 0f);
+		}
+		//Move the camera along with the player but offset it
+		Camera.main.transform.position = transform.position + new Vector3 (0f, 1.98f, -3.57f);
+	}
+}
