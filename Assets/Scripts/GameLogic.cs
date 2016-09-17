ac434cd4c55838d57eb1dc1e1187a10859e158cb
Rahul Joshi <rahuljoshi1997@gmail.com>
2016-09-16 20:41:03 -0400

    Have done it all. All the other stops are there. Their assets are in. Have the UI text displaying directions and have the camera flash in the end.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,22 +4,81 @@ using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour {
 
-	bool didPlayerWin = false;
-	public Text winText;
+	public bool didPlayerWin = false;
+	bool rPress = false;
+
+	public Text messageText;
+	public Image flash;
+
 	public Transform player;
-	public Transform treasure;
+
+	public Transform point1;
+	public Transform point2;
+	public Transform point3;
+	public Transform point4;
+	public Transform point5;
 
 	// Use this for initialization
 	void Start () {
-		
+		flash.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Point0 --> the starting point
+		if ((player.position - Vector3.zero).magnitude < 10f) {
+			messageText.text = "You must get the camera";
+		} else if ((player.position - Vector3.zero).magnitude < 20f) {
+			messageText.text = "";
+		}
+
+		//Point1
+		if ((player.position - point1.position).magnitude < 20f) {
+			if (rPress == false) {
+				messageText.text = "Hold [R] and move the mouse to look around";
+			} if (Input.GetKeyUp (KeyCode.R)) {
+				rPress = true;
+				messageText.text = "You decide to head South West towards the Solid Rocket Booster";
+			}
+		} else if ((player.position - point1.position).magnitude < 30f) {
+			messageText.text = "";
+		}
+
+		//Point2
+		if ((player.position - point2.position).magnitude < 10f) {
+			messageText.text = "To the South, you see an asteroid.";
+		} else if ((player.position - point2.position).magnitude < 20f) {
+			messageText.text = "";
+		}
+
+		//Point3
+		if ((player.position - point3.position).magnitude < 10f) {
+			messageText.text = "You go East to a Disco Ball";
+		} else if ((player.position - point3.position).magnitude < 20f) {
+			messageText.text = "";
+		}
+
+		//Point4
+		if ((player.position - point4.position).magnitude < 10f) {
+			messageText.text = "South. It's hard to see, but the Camera is there";
+		} else if ((player.position - point4.position).magnitude < 20f) {
+			messageText.text = "";
+		}
+
+		//Point5
 		if (didPlayerWin) {
-			winText.text = "You Win!";
-		} else if ((player.position - treasure.position).magnitude < 5f) {
-			winText.text = "Press [SPACE] to Win";
+			if ((player.position - point5.position).magnitude < 40f) {
+				messageText.text = "With photos taken, you and your camera drift off in space";
+			} else {
+				messageText.text = "";
+			}
+			flash.enabled = true;
+
+			Color col = flash.color;
+			col.a -= 0.7f * Time.deltaTime;
+			flash.color = col;
+		} else if ((player.position - point5.position).magnitude < 5f) {
+			messageText.text = "Press [SPACE] to snap a photo";
 			if (Input.GetKey(KeyCode.Space)) {
 				didPlayerWin = true;
 			}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -14,37 +14,49 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Moving the player
-		if (Input.GetKey (KeyCode.W)) {
-			transform.position += new Vector3 (0f, 0f, movementSpeed) * Time.deltaTime;
-			transform.eulerAngles = new Vector3 (90f, 0f, 0f);
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			transform.position += new Vector3 (0f, 0f, -movementSpeed) * Time.deltaTime;
-			transform.eulerAngles = new Vector3 (90f, 180f, 0f);
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			transform.position += new Vector3 (-movementSpeed, 0f, 0f) * Time.deltaTime;
-			transform.eulerAngles = new Vector3 (90f, -90f, 0f);
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			transform.position += new Vector3 (movementSpeed, 0f, 0f) * Time.deltaTime;
-			transform.eulerAngles = new Vector3 (90f, 90f, 0f);
-		}
-		//Move the camera along with the player but offset it
-		Camera.main.transform.position = transform.position + new Vector3 (0f, 1.98f, -3.57f);
+		//Player can move only when they've not won yet. Movement disabled when they win
+		if (GameObject.Find ("Audio-NoCursor-GameLogic").GetComponent<GameLogic> ().didPlayerWin == false) {
+			//Moving the player
+			if (Input.GetKey (KeyCode.W)) {
+				transform.position += new Vector3 (0f, 0f, movementSpeed) * Time.deltaTime;
+				transform.eulerAngles = new Vector3 (90f, 0f, 0f);
+			}
+			if (Input.GetKey (KeyCode.S)) {
+				transform.position += new Vector3 (0f, 0f, -movementSpeed) * Time.deltaTime;
+				transform.eulerAngles = new Vector3 (90f, 180f, 0f);
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				transform.position += new Vector3 (-movementSpeed, 0f, 0f) * Time.deltaTime;
+				transform.eulerAngles = new Vector3 (90f, -90f, 0f);
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				transform.position += new Vector3 (movementSpeed, 0f, 0f) * Time.deltaTime;
+				transform.eulerAngles = new Vector3 (90f, 90f, 0f);
+			}
+			//Move the camera along with the player but offset it
+			Camera.main.transform.position = transform.position + new Vector3 (0f, 1.98f, -3.57f);
 
-		//Rotating the Camera
-		float rotateAxisY = 0f;
-		float rotateAxisX = 0f;
+			//Rotating the Camera
+			float rotateAxisY = 0f;
+			float rotateAxisX = 0f;
 
-		rotateAxisY += rotationSpeed * Input.GetAxis ("Mouse X");
-		rotateAxisX += rotationSpeed * Input.GetAxis ("Mouse Y");
-		//Camera.main.transform.eulerAngles = new Vector3 (rotateX, rotateY, 0f);
-		Camera.main.transform.Rotate(new Vector3 (-rotateAxisX, rotateAxisY, 0f));
-		//Reset camera
-		if (Input.GetKeyDown (KeyCode.R)) {
-			Camera.main.transform.eulerAngles = new Vector3 (17.734f, 0f, 0f);
+			if (Input.GetKey (KeyCode.R)) {
+				rotateAxisY += rotationSpeed * Input.GetAxis ("Mouse X");
+				rotateAxisX += rotationSpeed * Input.GetAxis ("Mouse Y");
+				//Camera.main.transform.eulerAngles = new Vector3 (rotateX, rotateY, 0f);
+				Camera.main.transform.Rotate (new Vector3 (-rotateAxisX, rotateAxisY, 0f));
+			} else {
+				//Reset camera
+				Camera.main.transform.eulerAngles = new Vector3 (17.734f, 0f, 0f);
+			}
+		}
+
+		//Make the player drift off into space when the game is over
+		if (GameObject.Find ("Audio-NoCursor-GameLogic").GetComponent<GameLogic> ().didPlayerWin == true) {
+			//Move player SouthWest
+			transform.position += new Vector3 (-movementSpeed/2f, 0f, -movementSpeed/2f) * Time.deltaTime;
+			//Move the camera along with the player but offset it
+			Camera.main.transform.position = transform.position + new Vector3 (0f, 1.98f, -3.57f);
 		}
 	}
 }
